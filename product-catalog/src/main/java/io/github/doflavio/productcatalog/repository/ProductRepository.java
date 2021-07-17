0d2d0a8615d5854def3bd66f182b9f76af3afd3b
Flávio Leandro Pereira <flavio.pereira@prodemge.gov.br>
2021-07-16 22:26:39 -0300

    criação do repository

---
+++ product-catalog/src/main/java/io/github/doflavio/productcatalog/repository/ProductRepository.java
@@ -0,0 +1,12 @@
+package io.github.doflavio.productcatalog.repository;
+
+import io.github.doflavio.productcatalog.model.Product;
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.List;
+
+public interface ProductRepository extends CrudRepository<Product, Integer> {
+
+    List<Product> findByName(String name);
+
+}
